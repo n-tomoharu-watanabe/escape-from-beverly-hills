8dbbc9eab7c8fd515316a4bdebee98ce606a0d3d
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-09-08 11:22:45 +0900

    不要なファイルを削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>ビバリーヒルズからの脱出</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+      </Head>
+
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
-export default MyApp
+
+export default App
