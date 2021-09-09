389bab00d699c9aa88524f8890986769e2c2e17a
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-09-09 09:16:37 +0900

    水平スクロールを試作

---
+++ pages/horizon-sccroll.tsx
@@ -0,0 +1,25 @@
+function range(length: number) {
+  return Array.from({length}, (_, i) => i)
+}
+
+const HorizonCcroll = () => {
+  return (
+    <div className="flex overflow-x-auto" style={{ scrollSnapType: "x mandatory" }}>
+      {range(10).map(i => (
+        <div className={`w-screen h-screen bg-${i % 2 ? 'red' : 'green'}-600 flex justify-center items-center`} style={{ scrollSnapAlign: "start" }}>
+          <div className="w-screen h-screen flex justify-center items-center">
+            Hello
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export const Page = () => {
+  return (
+    <HorizonCcroll />
+  )
+}
+
+export default Page
