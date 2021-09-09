f81f18a26c73f86dd523b8c27178232ae0506f8c
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-09-09 18:48:25 +0900

    keyの入れ忘れを修正

--- pages/horizon-sccroll.tsx
+++ pages/horizon-sccroll.tsx
@@ -6,7 +6,7 @@ const HorizonCcroll = () => {
   return (
     <div className="flex overflow-x-auto" style={{ scrollSnapType: "x mandatory" }}>
       {range(10).map(i => (
-        <div className={`w-screen h-screen bg-${i % 2 ? 'red' : 'green'}-600 flex justify-center items-center`} style={{ scrollSnapAlign: "start" }}>
+        <div className={`w-screen h-screen bg-${i % 2 ? 'red' : 'green'}-600 flex justify-center items-center`} style={{ scrollSnapAlign: "start" }} key={i}>
           <div className="w-screen h-screen flex justify-center items-center">
             Hello
           </div>
