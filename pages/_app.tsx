b57dbf60e7841dae8a2c0dcc2f513a42aac04cac
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-09-09 17:35:05 +0900

    tailwindcssをCDNからPostCSSに移行

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import "../styles/globals.css"
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
         <title>ビバリーヒルズからの脱出</title>
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
       </Head>
 
       <div>
