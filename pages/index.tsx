e1b3fb85f393c00056eb44b3864c18a68c9f9700
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-09-10 09:23:03 +0900

    index.tsxに試作を掲載

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,29 @@
+import React from "react"
+import { HorizonCcroll } from "./horizon-sccroll"
+
+function range(length: number) {
+  return Array.from({ length }, (_, i) => i)
+}
+
+function importAll(context: __WebpackModuleApi.RequireContext) {
+  return context.keys().map((e: string) => context(e))
+}
+
+interface MDXModule {
+  default: React.VFC<any>[]
+}
+
 export const Page = () => {
-  return (<div>Hello!</div>)
+  const modules: MDXModule[] = importAll(require.context("../assets/mdx/steps"))
+
+  const steps = range(modules.length + 1).map(i => () => <div>Step{i + 1}</div>)
+  const sections = steps.map((step, i) => [step, ...(modules[i]?.default ?? [])]).flat()
+
+  return (
+    <HorizonCcroll>
+      {sections.map(Section => <Section />)}
+    </HorizonCcroll>
+  )
 }
 
 export default Page
